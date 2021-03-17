520262a810b612fcc9595ab412ab27a5123e9fc6
Filip Svoboda <st58611@upce.cz>
2021-03-18 00:09:09 +0100

    Přidejte soubory projektu.

---
+++ CV04_PR1_FILIP_SVOBODA/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CV04_PR1_FILIP_SVOBODA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
